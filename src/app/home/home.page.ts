193254ce4db0ee9b308ebb82a89337968f7a12fd
karthik <skarthi@veemak.com>
2021-08-31 13:20:36 +0530

    master

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,105 @@
-import { Component } from '@angular/core';
-
+/* eslint-disable object-shorthand */
+/* eslint-disable no-underscore-dangle */
+import { Component, OnDestroy, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, ViewChild  } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AudioRecordingService } from '../audio-recording.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy{
 
-  constructor() {}
+  @ViewChild('videoElement') videoElement: any;
+  video: any;
+  isPlaying = false;
+  displayControls = true;
+  isAudioRecording = false;
+  isVideoRecording = false;
+  audioRecordedTime;
+  videoRecordedTime;
+  audioBlobUrl;
+  videoBlobUrl;
+  audioBlob;
+  videoBlob;
+  audioName;
+  videoName;
+  audioStream;
+  videoStream: MediaStream;
+  audioConf = { audio: true};
+  videoConf = { video: { facingMode:'user', width: 320 }, audio: true};
+
+
+  constructor(
+    private ref: ChangeDetectorRef,
+    private sanitizer: DomSanitizer,
+    private audioRecordingService: AudioRecordingService,
+  ) {
+    this.audioRecordingService.recordingFailed().subscribe(() => {
+      this.isAudioRecording = false;
+      this.ref.detectChanges();
+ });
+     this.audioRecordingService.getRecordedTime().subscribe((time) => {
+      this.audioRecordedTime = time;
+      this.ref.detectChanges();
+    });
+
+    this.audioRecordingService.getRecordedBlob().subscribe((data) => {
+      this.audioBlob = data.blob;
+      this.audioName = data.title;
+      this.audioBlobUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(data.blob));
+      this.ref.detectChanges();
+    });
+  }
+  ngOnInit(): void {
+  }
+
+  startAudioRecording() {
+    if (!this.isAudioRecording) {
+      this.isAudioRecording = true;
+      this.audioRecordingService.startRecording();
+    }
+  }
+
+  abortAudioRecording() {
+    if (this.isAudioRecording) {
+      this.isAudioRecording = false;
+      this.audioRecordingService.abortRecording();
+    }
+  }
+
+  stopAudioRecording() {
+    if (this.isAudioRecording) {
+      this.audioRecordingService.stopRecording();
+      this.isAudioRecording = false;
+    }
+  }
+
+  clearAudioRecordedData() {
+    this.audioBlobUrl = null;
+  }
+
+  downloadAudioRecordedData() {
+    this._downloadFile(this.audioBlob, 'audio/mp3', this.audioName);
+  }
+
+  ngOnDestroy(): void {
+    this.abortAudioRecording();
+  }
+
+  _downloadFile(data: any, type: string, filename: string): any {
+    const blob = new Blob([data], { type: type });
+    const url = window.URL.createObjectURL(blob);
+    //this.video.srcObject = stream;
+    //const url = data;
+    const anchor = document.createElement('a');
+    anchor.download = filename;
+    anchor.href = url;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  }
+
 
 }
